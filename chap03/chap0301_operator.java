7dd96d4530debf824e695810b8c9d4e0455392cc
bbiyak25 <officialbbiyak@gmail.com>
2026-02-05 10:54:01 +0000

    휴 살았다

--- chap03/chap0301_operator.java
+++ chap03/chap0301_operator.java
@@ -24,7 +24,38 @@ public class chap0301_operator {
         k = f/g;        // 정수 몫
         k = f%g;        // 정수 나머지
 
-        k = f**2;
+        char c1 = 'A'+1; // int 계산, 65+1 = 66, B가 저장됨
+        int d2 = c1 +2;     // int 계산, 67 나옴
+
+        String s1 = "happy"+7.0;        // happy7.0 저장됨
+        
+        // String 비교연산자
+        String s2 = "안녕하세요";
+        String s3 = "햄수탑니다";
+        boolean w1 = s2.equals(s1);
+
+        // 논리연산자
+        boolean w2 = true;
+        boolean w3 = false;
+
+        boolean AND = w2 && w3;
+        boolean OR = w2 || w3;
+        boolean XOR = w2 ^ w3;      // 둘이 달라야홤
+        boolean NOT = !w2;
+
+        // 삼항연산자
+        int score = 90;
+        String ans;
+        if (score > 80) {
+            ans = "합격";
+        }
+        else {
+            ans ="불합격";
+        }
+
+        String answ = (score > 80) ? "합격" : "불합격";       // 한 줄로 줄여쓰기
+
+
         
 
     }
